69000b37877dba92dc7090432cd5ddc956ba9206
VitalyKalinsky <kalinskyvii@gmail.com>
2026-03-25 14:43:55 +0300

    started EDA

---
+++ EDA/test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import seaborn as sns
+import sklearn
+
+print(f"Pandas: {pd.__version__}")
+print(f"NumPy: {np.__version__}")
+print(f"Matplotlib: {matplotlib.__version__}")
+print(f"Seaborn: {sns.__version__}")
+print(f"Scikit-learn: {sklearn.__version__}")
